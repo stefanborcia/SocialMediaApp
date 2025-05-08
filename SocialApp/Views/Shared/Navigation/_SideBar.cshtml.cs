b79f7d9c77c8207c017d126fdb7c5391cff7eb81
stefanborcia <stefigabi89@yahoo.com>
2025-05-08 13:34:26 +0200

    Implemented SideBar and TopBar and PartialView

---
+++ SocialApp/Views/Shared/Navigation/_SideBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialApp.Navigation
+{
+    public class _SideBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SocialApp/Views/Shared/Navigation/_TopBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialApp.Navigation
+{
+    public class _TopBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
